54de17f3006075d1c7fc0c477db5b6cf69ce3fa2
Adrian Markefka <am97veb@gmail.com>
2026-01-29 14:15:23 +0100

    add element UsersListPage to Route

--- src/core/App.tsx
+++ src/core/App.tsx
@@ -1,13 +1,15 @@
 import { HashRouter, Navigate, Route, Routes } from "react-router-dom";
 import { toUsers } from "./routes";
 import { Navigation } from "./Navigation";
+import { UsersListPage } from "../features/UsersListPage";
 
 export const App = () => (
-  <HashRouter>
-    <Navigation />
-    <Routes>
-      <Route path={toUsers()} />
-      <Route path="/" element={<Navigate to={toUsers()} />} />
-    </Routes>
-  </HashRouter>
-);
+    <HashRouter>
+      <Navigation />
+      <Routes>
+        <Route path={toUsers()} element={<UsersListPage />} />
+        <Route path="/" element={<Navigate to={toUsers()} />} />
+      </Routes>
+    </HashRouter>
+  );
+
