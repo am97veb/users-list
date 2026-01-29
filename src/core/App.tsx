ae1d7399b725debbf145d50cd0011ffbe0264b55
Adrian Markefka <am97veb@gmail.com>
2026-01-29 10:02:46 +0100

    add Navigation to App routing

--- src/core/App.tsx
+++ src/core/App.tsx
@@ -1,8 +1,10 @@
 import { HashRouter, Navigate, Route, Routes } from "react-router-dom";
 import { toUsers } from "./routes";
+import { Navigation } from "./Navigation";
 
 export const App = () => (
   <HashRouter>
+    <Navigation />
     <Routes>
       <Route path={toUsers()} />
       <Route path="/" element={<Navigate to={toUsers()} />} />
