b2a4917c0ee5e7fd993989151a54423136ad6d0d
Adrian Markefka <am97veb@gmail.com>
2026-01-30 11:19:15 +0100

    add toSelectUser root with element UserPage

--- src/core/App.tsx
+++ src/core/App.tsx
@@ -1,10 +1,11 @@
 import { HashRouter, Navigate, Route, Routes } from "react-router-dom";
-import { toUsers } from "./routes";
+import { toSelectUser, toUsers } from "./routes";
 import { Navigation } from "./Navigation";
 import { UsersListPage } from "../features/UsersListPage";
 import { useAppDispatch } from "./hooks";
 import { useEffect } from "react";
 import { fetchUsers } from "../features/UsersListPage/usersThunk";
+import { UserPage } from "../features/UsersListPage/UserPage";
 
 export const App = () => {
   const dispatch = useAppDispatch();
@@ -18,6 +19,7 @@ export const App = () => {
       <Navigation />
       <Routes>
         <Route path={toUsers()} element={<UsersListPage />} />
+        <Route path={toSelectUser()} element={<UserPage />} />
         <Route path="/" element={<Navigate to={toUsers()} />} />
       </Routes>
     </HashRouter>
