a2515d77375ca333e0c1ef7efbcbe184e8c1f8d0
Adrian Markefka <am97veb@gmail.com>
2026-01-30 10:30:59 +0100

    change useFilterUsers hook name to useFilterUsersByAddress and FilterUserProps to FilterUsersByAddressProps

---
+++ src/features/UsersListPage/useFilterUsersByAddress.ts
@@ -0,0 +1,12 @@
+import type { FilterUsersByAddressProps } from "../types";
+
+export const useFilterUsersByAddress = ({
+  sortedUsers,
+  searchAddress,
+}: FilterUsersByAddressProps) => {
+  return sortedUsers.filter((user) =>
+    `${user.address.street} ${user.address.city}`
+      .toLowerCase()
+      .includes(searchAddress.toLowerCase()),
+  );
+};
