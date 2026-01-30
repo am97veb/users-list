689b8c8f18b2499f6197791cc62118a20b25801e
Adrian Markefka <am97veb@gmail.com>
2026-01-30 09:16:33 +0100

    add sort function

--- src/features/UsersListPage/index.tsx
+++ src/features/UsersListPage/index.tsx
@@ -1,19 +1,34 @@
+import { useState } from "react";
 import { useAppSelector } from "../../core/hooks";
 import { selectUsers } from "../../core/usersSlice";
 
 export const UsersListPage = () => {
   const usersList = useAppSelector(selectUsers);
+  const [sortOrder, setSortOrder] = useState<"asc" | "desc">("asc");
+
+  const sortedUsers = [...usersList].sort((a, b) => {
+    if (sortOrder === "asc") {
+      return a.name.localeCompare(b.name);
+    } else {
+      return b.name.localeCompare(a.name);
+    }
+  });
+
+  const toggleSort = () => {
+    setSortOrder(sortOrder === "asc" ? "desc" : "asc");
+  };
 
   return (
     <>
-      {usersList.map((user) => (
+      <button onClick={toggleSort}>sort</button>
+      {sortedUsers.map((user) => (
         <ul>
           <li key={user.id}>
-            {user.name} 
-            {user.email} 
-            {user.address.city} 
-            {user.address.zipcode}
-            {user.address.street}
+            {user.name} {" "}
+            {user.email}{" "}
+            {user.address.city}{" "}
+            {user.address.zipcode}{" "}
+            {user.address.street}{" "}
           </li>
         </ul>
       ))}
