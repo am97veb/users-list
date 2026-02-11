41e39c70eeabdea954347e003a33fa5619793f3a
Adrian Markefka <am97veb@gmail.com>
2026-02-11 17:45:22 +0100

    style: add GlobalStyle to main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,13 @@ import { createRoot } from "react-dom/client";
 import { App } from "./core/App.tsx";
 import { Provider } from "react-redux";
 import { store } from "./core/store.ts";
+import { GlobalStyle } from "./GlobalStyles.ts";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <Provider store={store}>
-    <App />
+      <GlobalStyle />
+      <App />
     </Provider>
   </StrictMode>,
 );
