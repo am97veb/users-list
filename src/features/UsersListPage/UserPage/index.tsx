07bb5a3bc4cbf9774cc04fbe2d303d488cf96355
Adrian Markefka <am97veb@gmail.com>
2026-02-19 18:35:40 +0100

    style: add Listm ListItem, Key, Value components to UserPage

--- src/features/UsersListPage/UserPage/index.tsx
+++ src/features/UsersListPage/UserPage/index.tsx
@@ -1,20 +1,21 @@
 import { useParams } from "react-router-dom";
 import { useAppSelector } from "../../../core/hooks";
 import { selectUserById } from "../usersSlice";
-import { mapUserData } from "./mapUserData";
+import { mapUserData } from "../helpers/mapUserData";
+import { List, ListItem, Key, Value } from "./styled";
 
 export const UserPage = () => {
   const { id } = useParams();
   const user = useAppSelector((state) => selectUserById(state, Number(id)));
 
   return (
-    <ul>
+    <List>
       {user &&
         Object.entries(mapUserData(user)).map(([key, value]) => (
-          <li key={key}>
-            <span>{key}</span>:<span>{value}</span>
-          </li>
+          <ListItem key={key}>
+            <Key>{key}</Key><Value>{value}</Value>
+          </ListItem>
         ))}
-    </ul>
+    </List>
   );
 };
---
+++ src/features/UsersListPage/UserPage/styled.ts
@@ -0,0 +1,27 @@
+import styled from "styled-components";
+
+export const List = styled.ul`
+  list-style: none;
+  padding: 0;
+  margin: 0 15px;
+`;
+
+export const ListItem = styled.li`
+  padding: 15px 0 10px;
+  border-bottom: 1px solid gray;
+  max-width: 500px;
+  display: flex;
+  justify-content: space-between;
+`;
+
+export const Key = styled.span`
+  text-align: left;
+  align-self: end;
+`;
+
+export const Value = styled.span`
+  text-align: right;
+  display: flex;
+  flex-direction: column;
+  white-space: pre-line;
+`;
