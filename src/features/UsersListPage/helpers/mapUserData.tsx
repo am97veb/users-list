013c74ad09621682cba44021198d0b9e860c00ad
Adrian Markefka <am97veb@gmail.com>
2026-02-19 18:42:12 +0100

    fix: remove unnecessary comma in address formatting

--- src/features/UsersListPage/helpers/mapUserData.tsx
+++ src/features/UsersListPage/helpers/mapUserData.tsx
@@ -5,7 +5,7 @@ export const mapUserData = (userData: User) =>( {
 "username": `${userData.username}`,
 "email": `${userData.email}`,
 "phone": `${userData.phone}`,
-"address": ` ${userData.address.street}, ${userData.address.suite} \n ${userData.address.zipcode} ${userData.address.city},`,
+"address": ` ${userData.address.street}, ${userData.address.suite} \n ${userData.address.zipcode} ${userData.address.city}`,
 "website": `${userData.website}`,
 "company": `${userData.company?.name}`,
 } )
