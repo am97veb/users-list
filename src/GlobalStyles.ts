210e22f8559e9114cbb196e783a274204b178c3f
Adrian Markefka <am97veb@gmail.com>
2026-02-27 20:07:12 +0100

    style: add body background and color in GlobalStyle

--- src/GlobalStyles.ts
+++ src/GlobalStyles.ts
@@ -1,4 +1,5 @@
 import { createGlobalStyle } from "styled-components";
+import appBackground from "./assets/appBackground.png";
 
 export const GlobalStyle = createGlobalStyle`
     html {
@@ -12,7 +13,9 @@ export const GlobalStyle = createGlobalStyle`
         font-family: "Nunito", sans-serif;
         margin: 0 auto;
         max-width: 1500px;
-
+        background-image: url("${appBackground}");
+        background-size: cover;
+        color: white;  
     }
 
     #root {
