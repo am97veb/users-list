68b6daf4a3438e86d181edf64a9dbdc2391be9b5
Adrian Markefka <am97veb@gmail.com>
2026-02-28 15:08:02 +0100

    feat: add initial state with User type without id

--- src/features/AddUsersPage/index.tsx
+++ src/features/AddUsersPage/index.tsx
@@ -1,4 +1,23 @@
+import { useState } from "react";
+import { type User } from "../UsersListPage/types";
 
 export const AddUsersPage = () => {
-
+  const [userData, setUserData] = useState<Omit<User, "id">>({
+    name: "",
+    username: "",
+    email: "",
+    phone: "",
+    address: {
+      city: "",
+      street: "",
+      zipcode: "",
+      suite: "",
+    },
+    company: {
+      name: "",
+      catchPhrase: "",
+      bs: "",
+    },
+    website: "",
+  });
 };
