7bda5c194ed784710d37042bb7d6f3485b3f1b4b
Adrian Markefka <am97veb@gmail.com>
2026-03-06 09:40:52 +0100

    feat: add error handling for missing user in UserPage

--- src/features/UsersListPage/UserPage/index.tsx
+++ src/features/UsersListPage/UserPage/index.tsx
@@ -3,10 +3,15 @@ import { useAppSelector } from "../../../core/hooks";
 import { selectUserById } from "../usersSlice";
 import { mapUserData } from "../helpers/mapUserData";
 import { List, ListItem, Key, Value } from "./styled";
+import { Error } from "../../../common/Error";
 
 export const UserPage = () => {
   const { id } = useParams();
   const user = useAppSelector((state) => selectUserById(state, id ? id : ""));
+
+  if (!user) {
+    return <Error message="Sorry, the user cannot be found." />;
+  }
 
   return (
     <List>
