b8c3b934e02b32d9194f1d835abb28ef639fa651
Adrian Markefka <am97veb@gmail.com>
2026-03-07 15:35:42 +0100

    refactor: replace form inputs with formFields config object

--- src/features/AddUsersPage/index.tsx
+++ src/features/AddUsersPage/index.tsx
@@ -5,126 +5,107 @@ import { useAddUserForm } from "./useAddUserForm";
 
 export const AddUsersPage = () => {
   const { userData, onFormSubmit, onInputChange } = useAddUserForm();
+
+  const formFields = {
+    "basic info": [
+      {
+        signature: "name",
+        name: "name",
+        type: "text",
+        required: true,
+        value: userData.name,
+      },
+      {
+        signature: "email",
+        name: "email",
+        type: "email",
+        required: true,
+        value: userData.email,
+      },
+      {
+        signature: "phone",
+        name: "phone",
+        type: "number",
+        required: true,
+        value: userData.phone,
+      },
+      {
+        signature: "website",
+        name: "website",
+        type: "text",
+        required: false,
+        value: userData.website,
+      },
+    ],
+    address: [
+      {
+        signature: "street",
+        name: "address.street",
+        type: "text",
+        required: true,
+        value: userData.address.street,
+      },
+      {
+        signature: "suite",
+        name: "address.suite",
+        type: "text",
+        required: false,
+        value: userData.address.suite,
+      },
+      {
+        signature: "zip code",
+        name: "address.zipcode",
+        type: "text",
+        required: true,
+        value: userData.address.zipcode,
+      },
+      {
+        signature: "city",
+        name: "address.city",
+        type: "text",
+        required: true,
+        value: userData.address.city,
+      },
+    ],
+    company: [
+      {
+        signature: "name",
+        name: "company.name",
+        type: "text",
+        required: false,
+        value: userData.company?.name,
+      },
+      {
+        signature: "catch phrase",
+        name: "company.catchPhrase",
+        required: false,
+        type: "text",
+        value: userData.company?.catchPhrase,
+      },
+    ],
+  };
+
   return (
     <Form onSubmit={onFormSubmit}>
-      <Fieldset>
-        <Legend>basic info</Legend>
+      {Object.entries(formFields).map(([group, fields]) => (
+        <Fieldset key={group}>
+          <Legend>{group}</Legend>
 
-        <Label>
-          <FieldSignature>name*:</FieldSignature>
-          <Input
-            name="name"
-            type="text"
-            required
-            value={userData.name}
-            onChange={onInputChange}
-          />
-        </Label>
+          {fields.map(({ signature, name, type, required, value }) => (
+            <Label key={name}>
+              <FieldSignature>{signature}</FieldSignature>
+              <Input
+                name={name}
+                type={type}
+                required={required}
+                value={value}
+                onChange={onInputChange}
+              />
+            </Label>
+          ))}
+        </Fieldset>
+      ))}
 
-        <Label>
-          <FieldSignature>email*:</FieldSignature>
-          <Input
-            name="email"
-            type="email"
-            required
-            value={userData.email}
-            onChange={onInputChange}
-          />
-        </Label>
-
-        <Label>
-          <FieldSignature>phone*:</FieldSignature>
-          <Input
-            name="phone"
-            type="number"
-            required
-            value={userData.phone}
-            onChange={onInputChange}
-          />
-        </Label>
-
-        <Label>
-          <FieldSignature>website:</FieldSignature>
-          <Input
-            name="website"
-            type="text"
-            value={userData.website}
-            onChange={onInputChange}
-          />
-        </Label>
-      </Fieldset>
-
-      <Fieldset>
-        <Legend>address</Legend>
-
-        <Label>
-          <FieldSignature>street*:</FieldSignature>
-          <Input
-            name="address.street"
-            type="text"
-            required
-            value={userData.address.street}
-            onChange={onInputChange}
-          />
-        </Label>
-
-        <Label>
-          <FieldSignature>suite:</FieldSignature>
-          <Input
-            name="address.suite"
-            type="text"
-            required
-            value={userData.address.suite}
-            onChange={onInputChange}
-          />
-        </Label>
-
-        <Label>
-          <FieldSignature>zip code*:</FieldSignature>
-          <Input
-            name="address.zipcode"
-            type="number"
-            required
-            value={userData.address.zipcode}
-            onChange={onInputChange}
-          />
-        </Label>
-
-        <Label>
-          <FieldSignature>city*:</FieldSignature>
-          <Input
-            name="address.city"
-            type="text"
-            required
-            value={userData.address.city}
-            onChange={onInputChange}
-          />
-        </Label>
-      </Fieldset>
-
-      <Fieldset>
-        <Legend>company</Legend>
-
-        <Label>
-          <FieldSignature>name:</FieldSignature>
-          <Input
-            name="company.name"
-            type="text"
-            value={userData.company?.name}
-            onChange={onInputChange}
-          />
-        </Label>
-
-        <Label>
-          <FieldSignature>catch phrase:</FieldSignature>
-          <Input
-            name="company.catchPhrase"
-            type="text"
-            value={userData.company?.catchPhrase}
-            onChange={onInputChange}
-          />
-        </Label>
-      </Fieldset>
       <p> * required fields </p>
       <Button addUserToListButton>add to users list</Button>
     </Form>
