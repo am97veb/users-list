ab818b4afa3e5d2dfb0133cc051ebce0528a3d58
Adrian Markefka <am97veb@gmail.com>
2026-03-08 09:30:05 +0100

    refactor: extract formFields config to separate file

---
+++ src/features/AddUsersPage/getFormFields.tsx
@@ -0,0 +1,80 @@
+import type { User } from "../UsersListPage/types";
+
+export const getFormFields = (userData: Omit<User, "id">) => ({
+  "basic info": [
+    {
+      signature: "name",
+      name: "name",
+      type: "text",
+      required: true,
+      value: userData.name,
+    },
+    {
+      signature: "email",
+      name: "email",
+      type: "email",
+      required: true,
+      value: userData.email,
+    },
+    {
+      signature: "phone",
+      name: "phone",
+      type: "number",
+      required: true,
+      value: userData.phone,
+    },
+    {
+      signature: "website",
+      name: "website",
+      type: "text",
+      required: false,
+      value: userData.website,
+    },
+  ],
+  address: [
+    {
+      signature: "street",
+      name: "address.street",
+      type: "text",
+      required: true,
+      value: userData.address.street,
+    },
+    {
+      signature: "suite",
+      name: "address.suite",
+      type: "text",
+      required: false,
+      value: userData.address.suite,
+    },
+    {
+      signature: "zip code",
+      name: "address.zipcode",
+      type: "text",
+      required: true,
+      value: userData.address.zipcode,
+    },
+    {
+      signature: "city",
+      name: "address.city",
+      type: "text",
+      required: true,
+      value: userData.address.city,
+    },
+  ],
+  company: [
+    {
+      signature: "name",
+      name: "company.name",
+      type: "text",
+      required: false,
+      value: userData.company?.name,
+    },
+    {
+      signature: "catch phrase",
+      name: "company.catchPhrase",
+      required: false,
+      type: "text",
+      value: userData.company?.catchPhrase,
+    },
+  ],
+});
--- src/features/AddUsersPage/index.tsx
+++ src/features/AddUsersPage/index.tsx
@@ -2,88 +2,11 @@ import { Form, Fieldset, FieldSignature, Label, Legend } from "./styled";
 import { Input } from "../../common/Input";
 import { Button } from "../../common/Button";
 import { useAddUserForm } from "./useAddUserForm";
+import { getFormFields } from "./getFormFields";
 
 export const AddUsersPage = () => {
   const { userData, onFormSubmit, onInputChange } = useAddUserForm();
-
-  const formFields = {
-    "basic info": [
-      {
-        signature: "name",
-        name: "name",
-        type: "text",
-        required: true,
-        value: userData.name,
-      },
-      {
-        signature: "email",
-        name: "email",
-        type: "email",
-        required: true,
-        value: userData.email,
-      },
-      {
-        signature: "phone",
-        name: "phone",
-        type: "number",
-        required: true,
-        value: userData.phone,
-      },
-      {
-        signature: "website",
-        name: "website",
-        type: "text",
-        required: false,
-        value: userData.website,
-      },
-    ],
-    address: [
-      {
-        signature: "street",
-        name: "address.street",
-        type: "text",
-        required: true,
-        value: userData.address.street,
-      },
-      {
-        signature: "suite",
-        name: "address.suite",
-        type: "text",
-        required: false,
-        value: userData.address.suite,
-      },
-      {
-        signature: "zip code",
-        name: "address.zipcode",
-        type: "text",
-        required: true,
-        value: userData.address.zipcode,
-      },
-      {
-        signature: "city",
-        name: "address.city",
-        type: "text",
-        required: true,
-        value: userData.address.city,
-      },
-    ],
-    company: [
-      {
-        signature: "name",
-        name: "company.name",
-        type: "text",
-        required: false,
-        value: userData.company?.name,
-      },
-      {
-        signature: "catch phrase",
-        name: "company.catchPhrase",
-        required: false,
-        type: "text",
-        value: userData.company?.catchPhrase,
-      },
-    ],
-  };
+  const formFields = getFormFields(userData);
 
   return (
     <Form onSubmit={onFormSubmit}>
