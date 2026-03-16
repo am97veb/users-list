508ec42c8921ce40f94b0cb0e585277a0c73749a
Adrian Markefka <am97veb@gmail.com>
2026-03-16 17:16:36 +0100

    feat: connect express server to PostgreSQL via Prisma

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,16 +1,19 @@
+import "dotenv/config";
 import express from "express";
+import { PrismaPg } from "@prisma/adapter-pg";
+import { PrismaClient } from "@prisma/client";
 
+const connectionString = `${process.env.DATABASE_URL}`;
+const adapter = new PrismaPg({ connectionString });
+const prisma = new PrismaClient({ adapter });
 const app = express();
 
-const users = [
-  { id: 1, name: "Janina Kowalski", email: "jan@example.com" },
-  { id: 2, name: "Anna Nowak", email: "anna@example.com" },
-];
 
-app.get("/users", (req, res) => {
+app.get("/users", async (req, res) => {
+  const users = await prisma.user.findMany();
   res.json(users);
 });
 
 app.listen(3000, () => {
-  console.log("Server running on http://localhost:3000");
+  console.log("Server running on http://localhost:3000/users");
 });
