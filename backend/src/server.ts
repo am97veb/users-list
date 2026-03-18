2ee10a64e3fc032464faba482d7fde40f9e9bb99
Adrian Markefka <am97veb@gmail.com>
2026-03-18 18:25:25 +0100

    feat: add POST /users endpoint

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,13 +1,43 @@
 import "dotenv/config";
 import express from "express";
 import { PrismaPg } from "@prisma/adapter-pg";
-import { PrismaClient } from "@prisma/client";
+import { PrismaClient } from "./generated/prisma/client.js";
 
 const connectionString = `${process.env.DATABASE_URL}`;
 const adapter = new PrismaPg({ connectionString });
 const prisma = new PrismaClient({ adapter });
 const app = express();
 
+app.use(express.json());
+
+app.post("/users", async (req, res) => {
+  const { name, email, phone, website, address, company } = req.body;
+
+  const user = await prisma.user.create({
+    data: {
+      name,
+      email,
+      phone,
+      website,
+      address: {
+        create: {
+          street: address.street,
+          suite: address.suite,
+          city: address.city,
+          zipcode: address.zipcode,
+        },
+      },
+      company: company
+      && {
+            create: {
+              name: company.name,
+              catchPhrase: company.catchPhrase,
+            },
+          },
+    },
+  });
+  res.status(201).json(user);
+});
 
 app.get("/users", async (req, res) => {
   const users = await prisma.user.findMany();
