9d3a9c035cffc52d69b946049fd4bb563b923950
Adrian Markefka <am97veb@gmail.com>
2026-04-13 18:48:58 +0200

    feat: add QueryClientProvider setup

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -5,13 +5,18 @@ import { Provider } from "react-redux";
 import { store } from "./core/store.ts";
 import { GlobalStyle } from "./GlobalStyles.ts";
 import { AppToastContianer } from "./ToastContainer/index.tsx";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const queryClient = new QueryClient();
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <Provider store={store}>
-      <GlobalStyle />
-      <AppToastContianer />
-      <App />
-    </Provider>
+    <QueryClientProvider client={queryClient}>
+      <Provider store={store}>
+        <GlobalStyle />
+        <AppToastContianer />
+        <App />
+      </Provider>
+    </QueryClientProvider>
   </StrictMode>,
 );
