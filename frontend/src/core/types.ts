11082d37d3a967e9b445b586a7f30f1d6b112e11
Adrian Markefka <am97veb@gmail.com>
2026-04-17 16:31:55 +0200

    refactor: remove Redux store and related users logic

--- frontend/src/core/types.ts
+++
@@ -1,7 +0,0 @@
-import type { User } from "../features/UsersListPage/types";
-
-export interface UsersState {
-  users: User[];
-  loading: boolean;
-  error: string | null;
-}
--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,8 +1,6 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { App } from "./core/App.tsx";
-import { Provider } from "react-redux";
-import { store } from "./core/store.ts";
 import { GlobalStyle } from "./GlobalStyles.ts";
 import { AppToastContianer } from "./ToastContainer/index.tsx";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
@@ -12,11 +10,9 @@ const queryClient = new QueryClient();
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <QueryClientProvider client={queryClient}>
-      <Provider store={store}>
         <GlobalStyle />
         <AppToastContianer />
         <App />
-      </Provider>
     </QueryClientProvider>
   </StrictMode>,
 );
