91cdc25cb7a3171bc236c7dce03122317360c201
Adrian Markefka <am97veb@gmail.com>
2026-04-21 12:52:24 +0200

    refactor: move ToastContainer to common

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,7 +2,7 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { App } from "./core/App.tsx";
 import { GlobalStyle } from "./GlobalStyles.ts";
-import { AppToastContianer } from "./ToastContainer/index.tsx";
+import { AppToastContianer } from "./common/ToastContainer/index.tsx";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 const queryClient = new QueryClient();
