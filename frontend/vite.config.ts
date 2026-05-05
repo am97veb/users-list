38f8a92bacbed80b06b058c36ce18d9bc0d78e84
Adrian Markefka <am97veb@gmail.com>
2026-05-05 15:34:24 +0200

    chore: add @ path alias for src directory

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,9 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import svgr from 'vite-plugin-svgr'
+import { resolve } from 'path/win32'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), svgr()],
   base: "/users-list/",
+  resolve: {
+    alias: {
+      "@": resolve(__dirname, "./src"),
+    },
+  },
 })
